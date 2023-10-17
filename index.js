0f7270c871b2a0f780430369f13cc0c2c3603417
francis <clyderitchie@yahoo.com>
2023-10-16 22:01:14 -0700

    Got the data i need

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-const inquirer = require('inquirer');
+// Example prompt
+// const inquirer = require('inquirer');
 
-inquirer
-    .promt([
-        {
-            type: 'input',
-            name: '', // what property should I add the answer to in the answer object
-            message: 'What is your first name?'
-        },
-        {
-            type: 'number',
-            name: '', // what property should I add the answer to in the answer object
-            message: 'How old are you?'
-        },
-        {
-            type: 'checkbox',
-            name: '', // what property should I add the answer to in the answer object
-            message: 'What tech skills do you have?',
-            choices: ['html', 'css', 'javascript']
-        },
-        {
-            type: 'commMethod',
-            name: '', // what property should I add the answer to in the answer object
-            message: 'What is your preference for receiving communication?',
-            choices: ['text', 'phone call', 'letter']
-        }
-    ]) 
-    .then (function(answer) {
-        console.log(answer);
-    });
+// inquirer
+//     .prompt([
+//         {
+//             type: 'input',
+//             message: 'What is your name?',
+//             name: 'fullName'
+//         },
+//         {
+//             type: 'list',
+//             message: 'What languages do you know?',
+//             name: 'languages',
+//             choices: ['html', 'css', 'javascript', 'react', 'node']
+//         },
+//         {
+//             type: 'list',
+//             message: 'What is your preferred method of communication?',
+//             name: 'commMethod',
+//             choices: ['text', 'phone call', 'email', 'letter']
+//         },
+//     ])
+//     .then(function(answer){
+//         console.log(answer);
+//     })
+
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
