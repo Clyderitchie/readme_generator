31d6a8c98b390a99af0981c0630640d7d0b33ba9
francis <clyderitchie@yahoo.com>
2023-10-17 18:20:55 -0700

    Added some code to hopefully generate the questions

--- index.js
+++ index.js
@@ -28,14 +28,81 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+        ([
+            {
+                type: 'input',
+                message: 'What is your project\'s name?',
+                name: 'title'
+            },
+            {
+                type: 'input',
+                message: 'Please write a short description of your project',
+                name: 'description'
+            },
+            {
+                type: 'list',
+                message: 'What kind of license should your project have?',
+                name: 'license',
+                choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: 'What command should be run to install dependencies?',
+                default: 'npm i',
+            },
+            {
+                type: 'input',
+                name: 'test',
+                message: 'What command should be run to run tests?',
+                default: 'npm test',
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'Provide usage instructions'
+            },
+            {
+                type: 'input',
+                name: 'contributing',
+                message: 'If contributing is allowed, how does one contribute',
+                default: 'Fork the project and create a pull request with your new code',
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is your github username?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your email address?'
+            }
+
+        ])
+        (function(answer){
+            console.log(answer);
+        })
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(answer => {
+        fs.writeFile('README.md', generateMarkdown(data), (err) => {
+            if (err) {
+                console.error(err)
+            }
+
+            console.log('Success! Your file is located in the dist folder!')
+        })
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,38 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== none) {
+    return ` ![Github license](https://img.shields.io/badge/license-${license.replace(/ /g, '%20')}-blue.svg)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## ${data.description}
 
+  ## ${data.license}
+
+  ## ${data.installation}
+
+  ## ${data.test}
+
+  ## ${data.usage}
+
+  ## ${data.contributing}
+
+  ## ${data.github}
+
+  ## ${data.email}
 `;
 }
 
